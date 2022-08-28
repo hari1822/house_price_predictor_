0350341c2160d974e05ed3589c39c098455449a7
hari202-2 <hari20221822@gmail.com>
2022-08-28 13:39:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import pickle
+from flask import Flask, render_template, request , jsonify
+import joblib
+from pyforest import*
+
+
+
+
+app= Flask(__name__)
+model = joblib.load(open("model.pkl", "rb"))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    try:
+     init=[float(x) for x in request.form.values()]
+     final=[np.array(init)]
+     prediction=model.predict(final)
+     output=round(prediction[0],2)
+     return render_template("index.html",prediction_text=f"The price of house {output}/-")
+    except:
+        return render_template("index.html",prediction_text=f"Invalid input")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
